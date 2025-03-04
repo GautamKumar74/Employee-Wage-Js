c966551819fc503f82aa8234d5a4f6ccf1d54963
Gautam Kumar <gk62018@gmail.com>
2025-03-04 12:25:32 +0530

    [Gautam] Add . employee wage calculation according to working hours

--- EmployeeWages.js
+++ EmployeeWages.js
@@ -6,3 +6,26 @@ if (empCheck === IS_PRESENT) {
 } else {
     console.log("Employee is Absent");
 }
+
+// UC2 - Calculate Daily Employee Wage
+const PART_TIME = 1;
+const FULL_TIME = 2;
+const PART_TIME_HOURS = 4;
+const FULL_TIME_HOURS = 8;
+const WAGE_PER_HOUR = 20;
+
+function getWorkHours(empType) {
+    switch (empType) {
+        case PART_TIME:
+            return PART_TIME_HOURS;
+        case FULL_TIME:
+            return FULL_TIME_HOURS;
+        default:
+            return 0;
+    }
+}
+
+let empType = Math.floor(Math.random() * 3); // 0, 1, or 2
+let empHours = getWorkHours(empType);
+let empWage = empHours * WAGE_PER_HOUR;
+console.log("Employee Wage: $" + empWage);
