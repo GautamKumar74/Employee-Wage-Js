f0ea98dbc25738f8f13457a749eb0cd0b8ace703
Gautam Kumar <gk62018@gmail.com>
2025-03-04 12:58:06 +0530

    [Gautam] Refactor . function added to employee wage calculation according to working hours

--- EmployeeWages.js
+++ EmployeeWages.js
@@ -7,13 +7,16 @@ if (empCheck === IS_PRESENT) {
     console.log("Employee is Absent");
 }
 
-// UC2 - Calculate Daily Employee Wage
+// Constants for UC2 and UC3
 const PART_TIME = 1;
 const FULL_TIME = 2;
 const PART_TIME_HOURS = 4;
 const FULL_TIME_HOURS = 8;
 const WAGE_PER_HOUR = 20;
 
+/**
+ * Function to get work hours based on employee type.
+ */
 function getWorkHours(empType) {
     switch (empType) {
         case PART_TIME:
@@ -25,6 +28,7 @@ function getWorkHours(empType) {
     }
 }
 
+// UC3 - Using the function to get work hours
 let empType = Math.floor(Math.random() * 3); // 0, 1, or 2
 let empHours = getWorkHours(empType);
 let empWage = empHours * WAGE_PER_HOUR;
